a4a1cab8aa359b19df4cc07d5b6507444290c467
Krishan Pandita Raman (Supplier) <raman.krishan.pandita@supplier.volvo.com>
2019-02-06 22:49:39 +0530

    Added index.html and DB Script file for creating tables and inserting data

---
+++ src/main/java/com/raman/flightreservation/controllers/ReservationRestController.java
@@ -0,0 +1,10 @@
+package com.raman.flightreservation.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReservationRestController {
+
+	
+	
+}
