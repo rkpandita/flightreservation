e1b59d44ec13029f23c2bb8506beb04d1b01f133
Krishan Pandita Raman (Supplier) <raman.krishan.pandita@supplier.volvo.com>
2019-02-08 12:31:01 +0530

    Added REST controller

--- src/main/java/com/raman/flightreservation/controllers/ReservationRestController.java
+++ src/main/java/com/raman/flightreservation/controllers/ReservationRestController.java
@@ -1,10 +1,34 @@
 package com.raman.flightreservation.controllers;
 
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.raman.flightreservation.dto.ReservationUpdateRequest;
+import com.raman.flightreservation.entities.Reservation;
+import com.raman.flightreservation.repository.ReservationRepository;
 
 @RestController
 public class ReservationRestController {
 
+	@Autowired
+	ReservationRepository reservationRepository;
+
+	@RequestMapping("/reservations/{id}")
+	public Reservation findReservation(@PathVariable("id") Long id) {
+		return reservationRepository.findById(id).orElseThrow(() -> new EntityNotFoundException());
+	}
 	
-	
+	@RequestMapping("/reservations")
+	public Reservation updateReservation(@RequestBody ReservationUpdateRequest request) {
+		Reservation reservation = reservationRepository.findById(request.getId()).orElseThrow(() -> new EntityNotFoundException());
+		reservation.setNoOfBags(request.getNoOfBags());
+		reservation.setCheckedIn(request.getCheckedIn());
+		return reservationRepository.save(reservation);
+	}
+
 }
---
+++ src/main/java/com/raman/flightreservation/dto/ReservationUpdateRequest.java
@@ -0,0 +1,33 @@
+package com.raman.flightreservation.dto;
+
+public class ReservationUpdateRequest {
+
+	private Long id;
+	private Boolean checkedIn;
+	private int noOfBags;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Boolean getCheckedIn() {
+		return checkedIn;
+	}
+
+	public void setCheckedIn(Boolean checkedIn) {
+		this.checkedIn = checkedIn;
+	}
+
+	public int getNoOfBags() {
+		return noOfBags;
+	}
+
+	public void setNoOfBags(int noOfBags) {
+		this.noOfBags = noOfBags;
+	}
+
+}
